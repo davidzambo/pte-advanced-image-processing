c77ffc8cf5e775d1ec2ffd5368d1f6d0a358d42b
David Zambo <dave@dcmf.io>
2022-05-04 21:53:21 +0200

    Testing with uncapped bottle

--- challenge_1/solution.py
+++ challenge_1/solution.py
@@ -2,18 +2,23 @@ from  PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
-image = Image.open('./images/image001.jpg')
+image = Image.open('./images/image008.jpg')
 
 image_array = np.array(image)
 image_width = len(image_array[0])
 one_third_of_width = image_width // 3
 
-middle_bottle_cap = image_array[:60, one_third_of_width:one_third_of_width * 2, :]
-
+middle_bottle_cap_green_component = image_array[:60, one_third_of_width:one_third_of_width * 2, 1]
+print(middle_bottle_cap_green_component[20][60])
 plt.figure()
-plt.imshow(middle_bottle_cap)
+plt.imshow(middle_bottle_cap_green_component)
 
-u = np.arange(0,3,1)+np.arange(0,9,2)[:,np.newaxis]
+
+middle_bottle_cap_blue_component = image_array[:60, one_third_of_width:one_third_of_width * 2, 2]
+print(middle_bottle_cap_blue_component[20][60])
+plt.figure()
+plt.imshow(middle_bottle_cap_blue_component)
+
 
 
 # image_grayscale_obj = image.convert("L")
